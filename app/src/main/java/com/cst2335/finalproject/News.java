c461f4629df4308921c83927c2a54c598396d3d0
mohamed <hass0403@algonquinlive.com>
2021-03-20 21:29:46 -0400

    first step soccer

---
+++ app/src/main/java/com/cst2335/finalproject/News.java
@@ -0,0 +1,16 @@
+package com.cst2335.finalproject;
+
+public class News {
+    String title;
+    String date;
+    String image;
+
+    News(){
+
+    }
+    News (String title, String date, String image){
+        this.title= title;
+        this.date= date;
+        this.image=image;
+    }
+}
