66304ac420bef0cbf326469003791cc1675bfc7b
Abdul-pyte <77282229+Abdul-pyte@users.noreply.github.com>
2021-04-13 14:05:32 -0400

    final push

--- app/src/main/java/com/cst2335/finalproject/TraiEmptyActivity.java
+++ app/src/main/java/com/cst2335/finalproject/TraiEmptyActivity.java
@@ -4,14 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class EmptyActivity extends AppCompatActivity {
+public class TraiEmptyActivity extends AppCompatActivity {
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_empty);
+        setContentView(R.layout.activity_empty_tri);
 
     }
 }
